41d51ebc4ad6cff105eb0b80a770df03d6be41d0
Rimel Morjane <37373286+RimelMj@users.noreply.github.com>
2025-11-28 15:54:34 +0100

    Update InsertBrick.php

--- src/Actions/InsertBrick.php
+++ src/Actions/InsertBrick.php
@@ -10,35 +10,39 @@ use Filament\Forms\Components\Actions\ButtonAction;
 
 class InsertBrick
 {
-    public static function make(Mason $component, Component $livewire): ButtonAction
+    public static function make(): ButtonAction
     {
         return ButtonAction::make('insertBrick')
             ->label(__('mason::mason.insert_brick'))
             ->modalWidth('sm')
             ->modalActionsAlignment('center')
-            ->form([
-                Select::make('name')
-                    ->label('Brick')
-                    ->placeholder('Select a brick')
-                    ->options(fn() => collect($component->getBricks())
-                        ->mapWithKeys(fn($brick) => [$brick->getName() => $brick->getLabel()])
-                    )
-                    ->searchable()
-                    ->required(),
-                Radio::make('position')
-                    ->label('Position')
-                    ->options([
-                        'before' => 'Before',
-                        'after' => 'After',
-                    ])
-                    ->inline()
-                    ->default('after')
-                    ->required()
-            ])
-            ->action(function (array $data) use ($component, $livewire) {
+            ->form(function (Mason $component) {
+                return [
+                    Select::make('name')
+                        ->label('Brick')
+                        ->placeholder('Select a brick')
+                        ->options(fn() => collect($component->getBricks())
+                            ->mapWithKeys(fn($brick) => [$brick->getName() => $brick->getLabel()])
+                        )
+                        ->searchable()
+                        ->required(),
+                    Radio::make('position')
+                        ->label('Position')
+                        ->options([
+                            'before' => 'Before',
+                            'after' => 'After',
+                        ])
+                        ->inline()
+                        ->default('after')
+                        ->required()
+                        ->extraAttributes(['style' => 'margin-bottom: 1rem;']),
+                ];
+            })
+            ->action(function (Mason $component, Component $livewire, array $data, array $arguments) {
                 $livewire->dispatch('handle-brick-insert', [
                     ...$data,
                     'statePath' => $component->getStatePath(),
+                    'editorSelection' => $arguments['editorSelection'] ?? null,
                 ]);
             });
     }
